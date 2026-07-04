8f4e99352b950f643e155e7041d57f0086ee0a3c
Roham Roohnavaz <roohnavazroham12@gmail.com>
2026-07-04 13:37:19 +0330

    Add project files.

---
+++ MyFinalProject.Domain/Entities/Abstraction/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFinalProject.Domain.Entities.Abstraction
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime ModifyAt { get; set; }
+    }
+}
